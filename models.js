8a35fc844a076e6092c26d009807a6d45ac342bc
Raju Mannem <rajumannem71@gmail.com>
2024-12-14 00:57:19 +0530

    model final v2

--- models.js
+++ models.js
@@ -1,34 +1,42 @@
 import mongoose from "mongoose";
 
-const consultantsScheema= new mongoose.Schema({
+const adminScheema= new mongoose.Schema({
     name: {type:String,required:true},
     email: {type:String,required:true},
     password: {type:String,required:true},
-});
-const consultantsDataScheema= new mongoose.Schema({
-    name: {type:String,required:true},
-    date: {type:String,required:true},
-    domain: {type:String,required:true},
-    client: {type:String,required:true},
-    vendor: {type:String,required:true},
-});
-const interviewExperienceScheema= new mongoose.Schema({
-    consultantName: {type:String,required:true},
-    date: {type:String,required:true},
-    client: {type:String,required:true},
-    skills: {type:String,required:true},
-    rating: {type:Number,required:true},
-    comments: {type:String,required:true},
 });
 const benchSalesScheema= new mongoose.Scheema({
     name: {type:String,required:true},
     email: {type:String,required:true},
     password: {type:String,required:true},
 });
-const adminScheema= new mongoose.Schema({
+const consultantsScheema= new mongoose.Schema({
     name: {type:String,required:true},
     email: {type:String,required:true},
     password: {type:String,required:true},
 });
+const consultantsDataScheema= new mongoose.Schema({
+    name: {type:String,required:true,unique:true},
+    domain: {type:String,required:true,unique:true},
+    experience: {type:String,required:true},
+    location: {type:String,required:true},
+    visa: {type:String,required:true},
+});
+const consultantsInteriewScheema= new mongoose.Schema({
+    consultantName: {type:String,required:true},
+    date: {type:String,required:true},
+    domain: {type:String,required:true},
+    client: {type:String,required:true},
+    vendor: {type:String,required:true},
+    skills: {type:String,required:true},
+    rating: {type:Number,required:true},
+    comments: {type:String,required:true},
+});
+
+const Admin=mongoose.model("Admin",adminScheema);
+const BenchSales=mongoose.model("BenchSales",benchSalesScheema);
 const Consultants=mongoose.model("Consultants",consultantsScheema);
-export {Consultants};
+const ConsultantsData=mongoose.model("ConsultantsData",consultantsDataScheema);
+const ConsultantsInterview=mongoose.model("ConsultantsInterview",consultantsInteriewScheema);
+
+export {Consultants,ConsultantsData,ConsultantsInterview,BenchSales,Admin};
